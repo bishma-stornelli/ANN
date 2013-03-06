a95af3075707d118e5708dcd4ad34e80eb2fdcce
Bishma Stornelli <bishma.stornelli@gmail.com>
2013-03-05 21:19:34 -0430

    Implementada la selección de carrusel y la elitista.

--- genetic-algorithms/gabil.rb
+++ genetic-algorithms/gabil.rb
@@ -1,63 +1,94 @@
-class gabil
+# population es un arreglo de tamano population_size de la forma
+# cuyos elementos son arreglos que representan un conjunto de reglas.
+# Cada elemento del conjunto es un arreglo que representa una simple regla
+# y cuyo tamano dependera del numero de valores que puedan tomar las variables
+# discretas asociadas al problema a resolver.
+# Para resumir:
+#   population[i] = i-esimo individuo (conjunto de reglas)
+#   population[i][j] = j-esima regla del i-esimo individuo
+class Gabil
 
-	attr_reader :population_size, :population, :inputs, :outputs, :fitness, :fatherSelection1, :fatherSelection2, :survivorSelection1, :survivorSelection2
+  attr_reader :population_size, :population, :inputs, :outputs, :fitness, :fatherSelection1, :fatherSelection2, :survivorSelection1, :survivorSelection2
 
 	attr_accessor :fitness_threshold, :n_features, :mutation_rate, :crossover_rate
 
-	def inicialize(fitness_threshold, n_features, population_size, mutation_rate = 0.001, crossover_rate = 0.6)
-		@population_size = population_size
-		@population = []
-		@inputs = []
-		@outputs = []
-		@fitness = []
-		@fatherSelection1 = []
-		@fatherSelection2 = []
-		@survivorSelection1 = []
-		@survivorSelection2 = []
-		@fitness_threshold = fitness_threshold
-		@n_features = n_features
-		@mutation_rate = mutation_rate
-		@crossover_rate = crossover_rate
+	def initialize(population, options = {} )
+	  options = {
+	    :population_size => 100,
+	    :fitness_threshold => 0.01,
+	    :mutation_rate => 0.001,
+	    :crossover_rate => 0.6,
+	    :selection_method => :roulette_wheel_selection
+	  }.merge(options)
+	  @population = population.dup
+		@population_size = options[:population_size]
+		@fitness_threshold = options[:fitness_threshold]
+		@mutation_rate = options[:mutation_rate]
+		@crossover_rate = options[:crossover_rate]
+		@current_fitness = calculate_fitness
 	end
-
-	def inicializePopulation()
-
+	
+	def evolve
+	  parents = selection
+	  crossover
+	  mutate
+	  update
+	  calculate_fitness
 	end
-
-  def selection(population)
-    send(@selection_method, population)
+	
+	# Delega la funcionalidad de seleccion al metodo @selection_method, pasandole
+	# el numero de padres a elegir para aparearse
+  def selection
+    send(@selection_method, ( (1 - @crossover_rate) * @population_size ))
   end
   
-	def roulette_wheel_selection(population)
-		selected = []    
-    current_fitness = []
+  # Selecciona new_size individuos probabilisticamente segun el fitness
+  # que aporta a la suma de fitness de toda la poblacion. Los individuos seleccionados
+  # son eliminados de @population
+	def roulette_wheel_selection!( new_size )
+		selected = []
     
-    for i in population do
+    for i in @population do
 			current_fitness << fitness(i)
 		end
 		
-		sum = current_fitness.inject(0.0){|res, item| res + item}
-		probabilities = current_fitness.map{ |i| i / sum }
-    
-    ( (1 - @crossover_rate) * @population_size ).times do 
-      # pick a random number and select the  individual 
+		sum = @current_fitness.inject(0.0){|res, item| res + item}
+		probabilities = @current_fitness.map{ |i| i / sum }
+		
+    new_size.times do 
+      # pick a random number and select the  h 
       # corresponding to that roulette-wheel area
       r , inc = rand * probabilities.max, 0 
-      population.each_index do |i| 
+      @population.each_index do |i| 
         if r < (inc += probabilities[i])
-          selected << population[i]
-          # make selection not pick sample twice
-          population.delete_at i
-          probabilities.delete_at i
+          selected << @population.delete_at(i)
+          probabilities.delete_at(i)
           break
         end
       end
     end
-		return selected
+    selected
 	end
 
-	def fatherSelection2(x)
-
+  # Selecciona new_size individuos de los cuales los new_size / 2 primeros son
+  # aquellos con mejor fitness y los restantes son seleccionados aleatoriamente.
+  # Elimina los elementos seleccionados de @population
+	def elitist_selection!( new_size )
+    selected = []
+    
+    sorted_population = @population.sort do |a, b| 
+      @current_fitness[@population.index(a)] <=> @current_fitness[@population.index(b)]
+    end
+    
+    (new_size / 2).times do
+      selected << @population.delete_at( 0 )
+    end
+    
+    while(selected.size < new_size )
+      selected << @population.delete_at( rand( @population.size) )
+    end
+    
+    selected
 	end
 
 	def survivorSelection1(x)
@@ -74,17 +105,6 @@ class gabil
 
 	def dropCondition
 
-	end
-
-  # Recibe el individuo con el cual se va a evaluar los ejemplos de entrenamiento
-  # Fitness(x) = (correct(x))^2
-	def fitness(x)
-
-	end
-	
-	# Devuelve el porcentaje de ejemplos clasificados correctamente usando el conjunto
-	# de reglas x
-	def correct(x)
 	end
 
 	def recombine(x)
@@ -106,5 +126,41 @@ class gabil
 			end
 		end
 	end
-
+	
+	private
+	
+	def calculate_fitness    
+    population.each do |h, index|
+			@current_fitness[index] = fitness(h)
+		end
+	end
+	
+	# Recibe el individuo con el cual se va a evaluar los ejemplos de entrenamiento
+  # fitness(h) = (correct(h))^2
+	def fitness(h)
+    (correct(h)) ** 2
+	end
+	
+	# Devuelve el porcentaje de ejemplos clasificados correctamente usando el conjunto
+	# de reglas h
+	def correct(h)
+	  correct = 0
+	  @training_examples.each do |example|
+	    correct += match_example?(h, example) ? 1 : 0
+	  end
+	end
+	
+	def match_example?(h, e)
+	  h.each do |rule|
+	    return true if match? rule, e
+	  end
+	  false
+	end
+	
+	def match?(rule, example)
+	  example.each_with_index do |e, i|
+	    return false if e == 1 && rule[i] == 0
+	  end
+    true
+	end
 end
